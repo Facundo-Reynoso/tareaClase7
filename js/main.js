417e82061d5f409125be90293d8630a43aa2c9b8
Facundo Reynoso <reynosofacundo1@hotmail.com>
2024-03-19 00:01:11 -0300

    soluciona error

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ function validarDescripcionRegalo(descripcionRegalo){
     if(descripcionRegalo.length === 0){
         return 'Este campo debe tener al menos un caracter'
     }
-    if(nombre.length >= 50){
+    if(descripcionRegalo.length >= 50){
         return 'Este campo debe tener menos de 50 caracteres'
     }
     return ''
