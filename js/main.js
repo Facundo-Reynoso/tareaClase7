c605fd2c9e60dd1defec474662a306603310a7c4
Facundo Reynoso <reynosofacundo1@hotmail.com>
2024-03-18 23:40:21 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,12 +9,12 @@ const ciudad = $form.ciudad.value //Esto esta maaaaaal
 const comportamiento = $form.comportamiento.value
 const descripcionRegalo = $form['descripcion-regalo'].value
 
-    function validarNombre(nombre){
-        if(nombre.length === 0){
-            return 'Este campo debe tener al menos un caracter'
-        }
-        if(nombre.length >= 50){
-            return 'Este campo debe tener menos de 50 caracteres'
-        }
-        return ''
+function validarNombre(nombre){
+    if(nombre.length === 0){
+        return 'Este campo debe tener al menos un caracter'
     }
+    if(nombre.length >= 50){
+        return 'Este campo debe tener menos de 50 caracteres'
+    }
+    return ''
+}
