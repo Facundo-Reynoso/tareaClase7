1ef1d9099d63b55e1a572dd11c1fd4c0fb30bf02
Facundo Reynoso <reynosofacundo1@hotmail.com>
2024-03-18 23:03:28 -0300

    clona repositorio

---
+++ js/main.js
@@ -0,0 +1,20 @@
+    const $form = document.querySelector('#carta-a-santa')
+
+    const nombre = $form.nombre.value
+    //tambien se puede usar
+    // const nombre = querySelector('[name=nombre]').value
+    // const nombre = querySelector('#nombre').value
+
+    const ciudad = $form.ciudad.value //Esto esta maaaaaal
+    const comportamiento = $form.comportamiento.value
+    const descripcionRegalo = $form['descripcion-regalo'].value
+
+    function validarNombre(nombre){
+        if(nombre.length === 0){
+            return 'Este campo debe tener al menos un caracter'
+        }
+        if(nombre.length >= 50){
+            return 'Este campo debe tener menos de 50 caracteres'
+        }
+        return ''
+    }
---
+++ js/pruebas.js
@@ -0,0 +1,12 @@
+function probarValidarNombre(){
+console.assert(
+    validarNombre('') === 'Este campo debe tener al menos un caracter',
+    'Validar nombre no validó que el nombre no sea vacio'
+ )
+
+ console.assert(
+    validarNombre('11111111111111111111111111111111111111111111111111') === 'Este campo debe tener menos de 50 caracteres',
+    'validar nombre no validó que el nombre tenga menos de 50 caracteres'
+ )
+}
+probarValidarNombre()
