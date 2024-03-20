5744716308ee7fc089b0bcbee8828d6f9655f1fd
Facundo Reynoso <reynosofacundo1@hotmail.com>
2024-03-20 05:01:45 -0300

    indentación

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,13 +1,13 @@
 function probarValidarNombre(){
-console.assert(
-    validarNombre('') === 'Este campo debe tener al menos un caracter',
-    'Validar nombre no validó que el nombre no sea vacio'
- )
+   console.assert(
+      validarNombre('') === 'Este campo debe tener al menos un caracter',
+      'Validar nombre no validó que el nombre no sea vacio'
+   )
 
- console.assert(
-    validarNombre('11111111111111111111111111111111111111111111111111') === 'Este campo debe tener menos de 50 caracteres',
-    'validar nombre no validó que el nombre tenga menos de 50 caracteres'
- )
+   console.assert(
+      validarNombre('11111111111111111111111111111111111111111111111111') === 'Este campo debe tener menos de 50 caracteres',
+      'validar nombre no validó que el nombre tenga menos de 50 caracteres'
+   )
 }
 probarValidarNombre()
 
